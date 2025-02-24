1cdfc70a425d1de046f85b6ac4a9715a29e8705e
HectorCavilla <hector.cavilla@gmail.com>
2025-02-24 12:26:33 -0500

    Deploy: First release of invite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import criticalCSS from "astro-critical-css";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [criticalCSS()]
+});
